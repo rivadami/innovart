7e27650f5303a061ff6165525736347537e198ed
cristianperalta <cristianeph@me.com>
2024-08-07 23:58:39 -0500

    Adding gallery library and home example

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,127 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { LayoutComponent } from '../../shared/layout/layout.component';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { Apollo } from 'apollo-angular';
+import { gql } from '@apollo/client/core';
+import { NgForOf, NgIf } from '@angular/common';
+import Flickity from 'flickity';
+import { GalleryHorizontalComponent } from '../../shared/gallery-horizontal/gallery-horizontal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     LayoutComponent,
-    HeaderComponent
+    HeaderComponent,
+    NgIf,
+    NgForOf,
+    GalleryHorizontalComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  @ViewChild('carousel') carousel!: ElementRef;
+  home: any;
+
+  constructor(private readonly apollo: Apollo) {
+  }
+
+  ngOnInit(): void {
+    this.apollo.watchQuery({
+      query: gql`
+        query HomeQuery {
+          page(id: "12", idType: DATABASE_ID) {
+            seo {
+              metaDesc
+              metaRobotsNofollow
+              metaRobotsNoindex
+              opengraphImage {
+                sourceUrl
+                mediaDetails {
+                  height
+                  width
+                  file
+                }
+              }
+              canonical
+              opengraphDescription
+              schema {
+                raw
+              }
+              opengraphSiteName
+              opengraphUrl
+              opengraphTitle
+              opengraphType
+            }
+            title
+            homeFields {
+              title
+              description
+              mainImage {
+                node {
+                  altText
+                  title
+                  sourceUrl
+                  srcSet
+                }
+              }
+              viewAllWorkLabel
+              selectedWorkLabel
+              selectedWork {
+                edges {
+                  node {
+                    ... on PortfolioCompany {
+                      title
+                      link
+                      uri
+                      portfolioSingleFields {
+                        galleryGrid {
+                          edges {
+                            node {
+                              title
+                              altText
+                              sourceUrl
+                              srcSet
+                            }
+                          }
+                        }
+                        title
+                        clientLocation
+                        description
+                        clientName
+                      }
+                    }
+                  }
+                }
+              }
+              testimonialsLabel
+              testimonials {
+                edges {
+                  node {
+                    ... on Testimonial {
+                      testimonialSingleFields {
+                        name
+                        title
+                        company
+                        description
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }`
+    }).valueChanges.subscribe((result: any) => {
+      console.log("@==>", result.data.page.homeFields);
+      this.home = result.data.page.homeFields;
+      setTimeout(() => this.setupGalleries(), 0);
+    });
+  }
+
+  setupGalleries() {
+
+  }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import { gql } from '@apollo/client/core';
+import { NgForOf } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  menu: string[] = [];
 
+  constructor(private readonly apollo: Apollo) {
+  }
+
+  ngOnInit(): void {
+    this.apollo.watchQuery({
+      query: gql`
+        query MainQuery {
+          globalContent {
+            globalFields {
+              siteLogo {
+                node {
+                  altText
+                  title
+                  sourceUrl
+                  srcSet
+                }
+              }
+              contactPhrase
+              contactPhraseLink
+              whoWeAre
+              phoneNumber {
+                target
+                title
+                url
+              }
+              emailAddress {
+                target
+                title
+                url
+              }
+              socialMedia {
+                socialMediaItem {
+                  target
+                  title
+                  url
+                }
+              }
+              footerLogo {
+                node {
+                  altText
+                  title
+                  sourceUrl
+                  srcSet
+                }
+              }
+              copyright
+              bookCall {
+                target
+                title
+                url
+              }
+            }
+          }
+          menus {
+            nodes {
+              name
+              menuItems {
+                edges {
+                  node {
+                    label
+                    url
+                    uri
+                    parentId
+                    target
+                  }
+                }
+              }
+            }
+          }
+        }`
+    }).valueChanges.subscribe((result: any) => {
+      console.log("@==>", result);
+      this.menu = result.data.menus.nodes;
+    });
+  }
 }
