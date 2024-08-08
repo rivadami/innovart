23588cf2ce85f066e5f097d587f64d6f8b1ddd2e
cristianperalta <cristianeph@me.com>
2024-08-07 23:58:51 -0500

    Adding gallery component

---
+++ src/app/shared/gallery-horizontal/gallery-horizontal.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import Flickity from 'flickity';
+
+@Component({
+  selector: 'app-gallery-horizontal',
+  standalone: true,
+    imports: [
+        NgForOf
+    ],
+  templateUrl: './gallery-horizontal.component.html',
+  styleUrl: './gallery-horizontal.component.scss'
+})
+export class GalleryHorizontalComponent implements AfterViewInit {
+  @ViewChild('carousel') carousel!: ElementRef;
+  @Input() gallery: any
+
+  ngAfterViewInit(): void {
+    const elem = this.carousel.nativeElement;
+    const flkty = new Flickity(elem, {
+      // options
+      cellAlign: 'left',
+      contain: true,
+      draggable: true
+    });
+  }
+}
