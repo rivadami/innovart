71b8d701c6df9b046661720ba11a522ceb20655c
cristianperalta <cristianeph@me.com>
2024-09-03 22:48:17 -0500

    Layering out portfolio posts

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,91 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { gql } from '@apollo/client/core';
+import { NgForOf, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
   portfolio: any;
+  noPosts  = 0;
+  hasMore = true;
 
   constructor(private readonly apollo: Apollo) {
   }
 
   ngOnInit(): void {
+    this.loadFirstN();
+  }
+
+  loadFirstN(noPosts: number = 4) {
     this.apollo.watchQuery({
-      query: gql`
-        query PortfolioQuery {
-          page(id: "69", idType: DATABASE_ID) {
-            seo {
-              title
-              metaDesc
-              metaRobotsNofollow
-              metaRobotsNoindex
-              opengraphImage {
-                sourceUrl
-                mediaDetails {
-                  height
-                  width
-                  file
-                }
-              }
-              canonical
-              opengraphDescription
-              schema {
-                raw
-              }
-              opengraphSiteName
-              opengraphUrl
-              opengraphTitle
-              opengraphType
-            }
-            title
-            portfolioFields {
-              title
-              description
-              projects(after: "", first: 4) {
-                edges {
-                  node {
-                    ... on PortfolioCompany {
-                      title
-                      link
-                      uri
-                      portfolioSingleFields {
-                        galleryGrid {
-                          edges {
-                            node {
-                              title
-                              altText
-                              sourceUrl
-                              srcSet
-                            }
-                          }
-                        }
+        query: gql`
+        query PortfolioPosts {
+          portfolioCompanies(first: ${noPosts}, where: {status: PUBLISH}, after: "") {
+            edges {
+              node {
+                title
+                link
+                uri
+                portfolioSingleFields {
+                  galleryGrid {
+                    edges {
+                      node {
                         title
-                        clientLocation
-                        description
-                        clientName
+                        altText
+                        sourceUrl
+                        srcSet
                       }
                     }
                   }
-                }
-                pageInfo {
-                  endCursor
-                  hasNextPage
+                  title
+                  clientLocation
+                  description
+                  clientName
                 }
               }
-              loadMore
+            }
+            pageInfo {
+              hasNextPage
+              endCursor
             }
           }
-        }`}
+        }
+      `
+      }
     ).valueChanges.subscribe((result: any) => {
-      console.log("@==>", result.data.page.homeFields);
-      this.portfolio = result.data.page.homeFields;
+      console.log("@==>", result.data.portfolioCompanies);
+      this.portfolio = result.data.portfolioCompanies;
+      this.noPosts = result.data.portfolioCompanies.edges.length;
+      this.hasMore = result.data.portfolioCompanies.pageInfo.hasNextPage;
     });
+  }
+
+  loadMore(noPosts: number = 4) {
+    this.loadFirstN(noPosts + 4);
   }
 
 }
