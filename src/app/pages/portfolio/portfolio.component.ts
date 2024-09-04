48b47c5bc6c96e00fa30b97d7f27d29c368c6596
cristianperalta <cristianeph@me.com>
2024-09-03 21:40:42 -0500

    Updating portfolio query

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { gql } from '@apollo/client/core';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,5 +10,82 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+  portfolio: any;
+
+  constructor(private readonly apollo: Apollo) {
+  }
+
+  ngOnInit(): void {
+    this.apollo.watchQuery({
+      query: gql`
+        query PortfolioQuery {
+          page(id: "69", idType: DATABASE_ID) {
+            seo {
+              title
+              metaDesc
+              metaRobotsNofollow
+              metaRobotsNoindex
+              opengraphImage {
+                sourceUrl
+                mediaDetails {
+                  height
+                  width
+                  file
+                }
+              }
+              canonical
+              opengraphDescription
+              schema {
+                raw
+              }
+              opengraphSiteName
+              opengraphUrl
+              opengraphTitle
+              opengraphType
+            }
+            title
+            portfolioFields {
+              title
+              description
+              projects(after: "", first: 4) {
+                edges {
+                  node {
+                    ... on PortfolioCompany {
+                      title
+                      link
+                      uri
+                      portfolioSingleFields {
+                        galleryGrid {
+                          edges {
+                            node {
+                              title
+                              altText
+                              sourceUrl
+                              srcSet
+                            }
+                          }
+                        }
+                        title
+                        clientLocation
+                        description
+                        clientName
+                      }
+                    }
+                  }
+                }
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                }
+              }
+              loadMore
+            }
+          }
+        }`}
+    ).valueChanges.subscribe((result: any) => {
+      console.log("@==>", result.data.page.homeFields);
+      this.portfolio = result.data.page.homeFields;
+    });
+  }
 
 }
