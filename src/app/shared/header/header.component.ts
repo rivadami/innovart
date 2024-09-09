893a239e8ab1c43d0c218c9f430ce48e74fed3f9
cristianperalta <cristianeph@me.com>
2024-09-08 22:12:25 -0500

    Adding angular materialˆ

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,14 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import { gql } from '@apollo/client/core';
-import { NgForOf } from '@angular/common';
+import { NgForOf, NgOptimizedImage } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { MainMenuComponent } from '../main-menu/main-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     RouterLink,
-    NgForOf
+    NgForOf,
+    NgOptimizedImage
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -17,7 +20,8 @@ import { NgForOf } from '@angular/common';
 export class HeaderComponent implements OnInit {
   menu: string[] = [];
 
-  constructor(private readonly apollo: Apollo) {
+  constructor(private readonly apollo: Apollo,
+              private dialog: MatDialog) {
   }
 
   ngOnInit(): void {
@@ -90,6 +94,17 @@ export class HeaderComponent implements OnInit {
     }).valueChanges.subscribe((result: any) => {
       console.log("@==>", result);
       this.menu = result.data.menus.nodes;
+    });
+  }
+
+  openMenu() {
+    this.dialog.open(MainMenuComponent, {
+      panelClass: 'fullscreen-dialog',
+      // height: '100vh',
+      // width: '100vw',
+      // position: {top: '0', left: '0'},
+      hasBackdrop: true,
+      backdropClass: 'custom-backdrop',
     });
   }
 }
