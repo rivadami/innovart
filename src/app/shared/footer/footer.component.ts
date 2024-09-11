cb6b7db45b772320a4882ef6bbb007c28021b10d
Damian Rivadulla <damian.rivadulla@gmail.com>
2024-09-11 18:41:55 -0300

    add footer Scrollrevel

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { LineRevealComponent } from '../line-reveal/line-reveal.component';
 import { ParagraphRevealComponent } from '../paragraph-reveal/paragraph-reveal.component';
+import ScrollReveal from 'scrollreveal';
+
 
 @Component({
   selector: 'app-footer',
@@ -12,6 +14,16 @@ import { ParagraphRevealComponent } from '../paragraph-reveal/paragraph-reveal.c
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
 
+export class FooterComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+
+    ScrollReveal().reveal('footer', {
+      interval: 200,
+      duration: 3000,
+      viewFactor: .1,
+    });
+
+  }
 }
