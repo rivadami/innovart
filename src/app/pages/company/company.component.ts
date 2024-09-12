d78c5589e0d8855aaa9449cc2a1087909e0056b1
Damian Rivadulla <damian.rivadulla@gmail.com>
2024-09-12 16:20:11 -0300

    company page

--- src/app/pages/company/company.component.ts
+++ src/app/pages/company/company.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../../shared/header/header.component';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { gql } from '@apollo/client/core';
+import { NgForOf, NgIf } from '@angular/common';
 import { LayoutComponent } from '../../shared/layout/layout.component';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { GalleryHorizontalComponent } from '../../shared/gallery-horizontal/gallery-horizontal.component';
+import { GalleryTextHorizontalComponent } from '../../shared/gallery-text-horizontal/gallery-text-horizontal.component';
+import { LineRevealComponent } from '../../shared/line-reveal/line-reveal.component';
+import { ParagraphRevealComponent } from '../../shared/paragraph-reveal/paragraph-reveal.component';
+import { CurtainRevealComponent } from '../../shared/curtain-reveal/curtain-reveal.component';
+import ScrollReveal from 'scrollreveal';
 
 @Component({
   selector: 'app-company',
   standalone: true,
     imports: [
+        LayoutComponent,
         HeaderComponent,
-        LayoutComponent
+        NgIf,
+        NgForOf,
+        GalleryHorizontalComponent,
+        GalleryTextHorizontalComponent,
+        LineRevealComponent,
+        ParagraphRevealComponent,
+        FooterComponent,
+        CurtainRevealComponent,          
     ],
   templateUrl: './company.component.html',
   styleUrl: './company.component.scss'
