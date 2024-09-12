ff8828196bb6dfdf384ee75953fd939de3ea2641
Damian Rivadulla <damian.rivadulla@gmail.com>
2024-09-12 17:54:27 -0300

    contact layout

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { gql } from '@apollo/client/core';
+import { NgForOf, NgIf } from '@angular/common';
+import { LayoutComponent } from '../../shared/layout/layout.component';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
+import { GalleryHorizontalComponent } from '../../shared/gallery-horizontal/gallery-horizontal.component';
+import { GalleryTextHorizontalComponent } from '../../shared/gallery-text-horizontal/gallery-text-horizontal.component';
+import { LineRevealComponent } from '../../shared/line-reveal/line-reveal.component';
+import { ParagraphRevealComponent } from '../../shared/paragraph-reveal/paragraph-reveal.component';
+import { CurtainRevealComponent } from '../../shared/curtain-reveal/curtain-reveal.component';
+import ScrollReveal from 'scrollreveal';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    LayoutComponent,
+    HeaderComponent,
+    NgIf,
+    NgForOf,
+    GalleryHorizontalComponent,
+    GalleryTextHorizontalComponent,
+    LineRevealComponent,
+    ParagraphRevealComponent,
+    FooterComponent,
+    CurtainRevealComponent,    
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
