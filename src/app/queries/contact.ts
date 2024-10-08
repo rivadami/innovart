dce02d332381a1c7bc9a5818cf5351b75031b334
Damian Rivadulla <damian.rivadulla@gmail.com>
2024-10-08 12:55:04 -0300

    contact fields

--- src/app/queries/contact.ts
+++ src/app/queries/contact.ts
@@ -1,7 +1,42 @@
 export const QUERY_CONTACT = `
-  query ContactQuery {
-    page(id: "68", idType: DATABASE_ID) {
+query ContactQuery {
+  page(id: "74", idType: DATABASE_ID) {
+    seo {
       title
+      metaDesc
+      metaRobotsNofollow
+      metaRobotsNoindex
+      opengraphImage {
+        sourceUrl
+        mediaDetails {
+          height
+          width
+          file
+        }
+      }
+      canonical
+      opengraphDescription
+      schema {
+        raw
+      }
+      opengraphSiteName
+      opengraphUrl
+      opengraphTitle
+      opengraphType
+    }
+    title
+    contactFields {
+      title
+      contactFormEmailTo
+      contactFormSubjectOptions {
+        option
+      }
+      howDidYouHearFromUs {
+        option
+      }
+      formNotice
+      formButton      
     }
   }
+}
 `;
