2c5b00a591bf39eb28dc31bf5f4e627764e1bc49
cristianperalta <cristianeph@me.com>
2024-10-13 16:17:29 -0500

    Adding contact form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { gql } from '@apollo/client/core';
-import { NgForOf, NgIf } from '@angular/common';
+import { JsonPipe, NgForOf, NgIf } from '@angular/common';
 import { LayoutComponent } from '../../shared/layout/layout.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
@@ -10,8 +10,9 @@ import { GalleryTextHorizontalComponent } from '../../shared/gallery-text-horizo
 import { LineRevealComponent } from '../../shared/line-reveal/line-reveal.component';
 import { ParagraphRevealComponent } from '../../shared/paragraph-reveal/paragraph-reveal.component';
 import { CurtainRevealComponent } from '../../shared/curtain-reveal/curtain-reveal.component';
-import { QUERY_CONTACT } from '../../queries/contact';
+import { MUTATION_SEND_EMAIL, QUERY_CONTACT } from '../../queries/contact';
 import { BaseComponentService } from '../../shared/services/base-component.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -27,13 +28,27 @@ import { BaseComponentService } from '../../shared/services/base-component.servi
     ParagraphRevealComponent,
     FooterComponent,
     CurtainRevealComponent,
+    ReactiveFormsModule,
+    JsonPipe,
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 
 export class ContactComponent extends BaseComponentService implements OnInit {
+  contactForm: FormGroup = new FormGroup<any>({
+    type: new FormControl(null, Validators.required),
+    name: new FormControl(null, Validators.required),
+    company: new FormControl(null, Validators.required),
+    position: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.email, Validators.required]),
+    phone: new FormControl(null, Validators.required),
+    hear: new FormControl(null, Validators.required),
+    message: new FormControl(null, Validators.required),
+  });
   contact: any;
+  error: boolean | null = null;
+  success: boolean | null = null;
 
   constructor(private readonly apollo: Apollo,
               elementRef: ElementRef,
@@ -41,8 +56,39 @@ export class ContactComponent extends BaseComponentService implements OnInit {
     super(elementRef, renderer);
   }
 
-  ngAfterViewInit(): void {
+  get formType() {
+    return this.contactForm.get('type');
+  }
 
+  get formName() {
+    return this.contactForm.get('name');
+  }
+
+  get formCompany() {
+    return this.contactForm.get('company');
+  }
+
+  get formPosition() {
+    return this.contactForm.get('position');
+  }
+
+  get formEmail() {
+    return this.contactForm.get('email');
+  }
+
+  get formPhone() {
+    return this.contactForm.get('phone');
+  }
+
+  get formHear() {
+    return this.contactForm.get('hear');
+  }
+
+  get formMessage() {
+    return this.contactForm.get('message');
+  }
+
+  ngAfterViewInit(): void {
     ScrollReveal().reveal('body', {
       interval: 200,
       duration: 1000,
@@ -57,6 +103,34 @@ export class ContactComponent extends BaseComponentService implements OnInit {
     }).valueChanges.subscribe((result: any) => {
       console.log("@==>", result.data.page);
       this.contact = result.data.page;
+    });
+    this.contactForm.markAllAsTouched();
+  }
+
+  sendForm() {
+    const form = this.contactForm.getRawValue();
+    console.log("@==>", form);
+    this.apollo.mutate({
+      mutation: gql`${MUTATION_SEND_EMAIL}`,
+      variables: {
+        email: form.email,
+        message: `
+          type: ${form.type},
+          name: ${form.name},
+          company: ${form.company},
+          position: ${form.position},
+          phone: ${form.phone},
+          hear: ${form.hear},
+          message: ${form.message},
+        `,
+        subject: "Contact page form submitted"
+      }
+    }).subscribe((response: any) => {
+      if (response.data.sendEmail.sent) {
+        this.success = true;
+      } else {
+        this.error = true;
+      }
     });
   }
 
--- src/app/queries/contact.ts
+++ src/app/queries/contact.ts
@@ -1,42 +1,61 @@
-export const QUERY_CONTACT = `
-query ContactQuery {
-  page(id: "74", idType: DATABASE_ID) {
-    seo {
-      title
-      metaDesc
-      metaRobotsNofollow
-      metaRobotsNoindex
-      opengraphImage {
-        sourceUrl
-        mediaDetails {
-          height
-          width
-          file
+export const MUTATION_SEND_EMAIL = `
+  mutation MyMutation($email: String, $message: String, $subject: String) {
+    sendEmail(
+        input: {
+          clientMutationId: "12345",
+          email: $email,
+          message: $message,
+          subject: $subject,
+          trap: "12345",
         }
-      }
-      canonical
-      opengraphDescription
-      schema {
-        raw
-      }
-      opengraphSiteName
-      opengraphUrl
-      opengraphTitle
-      opengraphType
-    }
-    title
-    contactFields {
-      title
-      contactFormEmailTo
-      contactFormSubjectOptions {
-        option
-      }
-      howDidYouHearFromUs {
-        option
-      }
-      formNotice
-      formButton      
+      ) {
+        email
+        message
+        subject
+        sent
     }
   }
-}
+`;
+
+export const QUERY_CONTACT = `
+  query ContactQuery {
+    page(id: "74", idType: DATABASE_ID) {
+      seo {
+        title
+        metaDesc
+        metaRobotsNofollow
+        metaRobotsNoindex
+        opengraphImage {
+          sourceUrl
+          mediaDetails {
+            height
+            width
+            file
+          }
+        }
+        canonical
+        opengraphDescription
+        schema {
+          raw
+        }
+        opengraphSiteName
+        opengraphUrl
+        opengraphTitle
+        opengraphType
+      }
+      title
+      contactFields {
+        title
+        contactFormEmailTo
+        contactFormSubjectOptions {
+          option
+        }
+        howDidYouHearFromUs {
+          option
+        }
+        formNotice
+        formButton
+      }
+    }
+  }
 `;
