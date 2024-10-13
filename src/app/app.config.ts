63240b25ad4d15fbf22847780a1d925714453848
cristianperalta <cristianeph@me.com>
2024-10-13 11:37:31 -0500

    Enabling hash navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -7,5 +7,5 @@ import { graphqlProvider } from './graphql.provider';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), graphqlProvider, provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes, withHashLocation()), provideHttpClient(), graphqlProvider, provideAnimationsAsync(), provideAnimationsAsync()]
 };
