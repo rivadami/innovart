958e7b2483a30c1c872d158c622ddf8273537336
Damian Rivadulla <damian.rivadulla@gmail.com>
2024-10-14 14:26:40 -0300

    update footer field

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -20,6 +20,7 @@ import { Apollo } from 'apollo-angular';
 
 export class FooterComponent implements OnInit, AfterViewInit {
   info: any = null;
+  footerInfo: any = null;
 
   constructor(private readonly apollo: Apollo) {
   }
@@ -29,6 +30,7 @@ export class FooterComponent implements OnInit, AfterViewInit {
       query: gql`${QUERY_HEADER}`
     }).valueChanges.subscribe((result: any) => {
       console.log("@==>", result);
+      this.footerInfo = result.data.globalContent.globalFields;
       this.info = result.data.menus.nodes;
     });
   }
